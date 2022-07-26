9fb520c64a497184d10049a541c818d03b433fc3
Rob Pungello <rob.pungello@gmail.com>
2022-07-26 15:32:48 -0400

    Add tests for API key creation

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -4,6 +4,7 @@ use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
 use Pdfsystems\WebDistributionSdk\Client;
+use Pdfsystems\WebDistributionSdk\Dtos\ApiKey;
 use Pdfsystems\WebDistributionSdk\Dtos\User;
 
 it('can create client', function () {
@@ -53,4 +54,31 @@ it('can authenticate via email/password', function () {
     expect($user->initials)->toBe('ABC');
     expect($user->name)->toBe('John Smith');
     expect($user->email)->toBe('john@example.com');
+});
+
+it('can create API keys', function () {
+    $mockUser = new User(
+        id: 1,
+        initials: 'ABC',
+        name: 'John Smith',
+        email: 'john@example.com'
+    );
+    $mockApiKey = new ApiKey(
+        id: 1,
+        name: 'API Key',
+        token: 'cafebabe'
+    );
+    $mock = new MockHandler([
+        new Response(200),
+        new Response(200, ['content-type' => 'application/json'], $mockUser->toJson()),
+        new Response(201, ['content-type' => 'application/json'], $mockApiKey->toJson()),
+    ]);
+    $client = new Client(handler: HandlerStack::create($mock));
+
+    $client->authenticateWithCredentials('john@example.com', 'password');
+    $apiKey = $client->createApiKey($mockApiKey);
+
+    expect($apiKey->id)->toBe(1);
+    expect($apiKey->name)->toBe('API Key');
+    expect($apiKey->token)->toBe('cafebabe');
 });
