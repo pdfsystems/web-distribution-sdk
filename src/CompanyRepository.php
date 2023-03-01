7ecc55d66c9d450405d777aa8bf21b6afcea2b99
Rob Pungello <rob@pdfsystems.com>
2023-03-01 10:08:28 -0500

    Add ability to load companies by ID

--- src/CompanyRepository.php
+++ src/CompanyRepository.php
@@ -3,7 +3,11 @@
 namespace Pdfsystems\WebDistributionSdk;
 
 use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\RequestException;
 use Pdfsystems\WebDistributionSdk\Dtos\Company;
+use Pdfsystems\WebDistributionSdk\Exceptions\ForbiddenException;
+use Pdfsystems\WebDistributionSdk\Exceptions\NotFoundException;
+use Pdfsystems\WebDistributionSdk\Exceptions\ResponseException;
 use Spatie\DataTransferObject\Exceptions\UnknownProperties;
 
 class CompanyRepository extends AbstractRepository
@@ -18,5 +22,34 @@ class CompanyRepository extends AbstractRepository
         return array_map(function (array $company): Company {
             return new Company($company);
         }, $this->client->getJson('api/company', ['with' => ['lines', 'currency']]));
+    }
+
+    /**
+     * @throws UnknownProperties
+     * @throws GuzzleException
+     * @throws ResponseException
+     */
+    public function findById(int $id): Company
+    {
+        $requestOptions = [
+            'with' => [
+                'lines',
+                'currency',
+            ],
+        ];
+
+        try {
+            $response = $this->client->getJson("api/company/$id", $requestOptions);
+
+            return new Company($response);
+        } catch (RequestException $e) {
+            if ($e->getCode() === 403) {
+                throw new ForbiddenException("You do not have permission to access company with id $id");
+            } elseif ($e->getCode() === 404 || $e->getCode() === 500) {
+                throw new NotFoundException("Company with id $id not found");
+            } else {
+                throw $e;
+            }
+        }
     }
 }
---
+++ src/Exceptions/ForbiddenException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Pdfsystems\WebDistributionSdk\Exceptions;
+
+class ForbiddenException extends ResponseException
+{
+}
