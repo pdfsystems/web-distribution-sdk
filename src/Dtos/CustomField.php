3b32b746a18ea30ac9436e7c96686852595ce9d6
Rob Pungello <rob@pdfsystems.com>
2024-10-24 11:05:19 -0400

    Cast custom field values based on type

--- src/Dtos/CustomField.php
+++ src/Dtos/CustomField.php
@@ -17,4 +17,24 @@ class CustomField extends AbstractDto
     public bool $ordertrack_filterable = false;
     /** @var string[] */
     public ?array $options = [];
+
+    public function toArray(): array
+    {
+        $array = parent::toArray();
+
+        switch ($this->type) {
+            case 2:
+                $array['current'] = intval($this->current);
+                break;
+            case 3:
+                $array['current'] = floatval($this->current);
+                break;
+            case 4:
+            case 5:
+                $array['current'] = boolval($this->current);
+                break;
+        }
+
+        return $array;
+    }
 }
