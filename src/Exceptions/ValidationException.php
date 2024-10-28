d0c29c2b281eafcb51838572677f0eaed45c7e14
Rob Pungello <rob@pdfsystems.com>
2024-10-28 16:17:20 -0400

    Nicer exception messages for ValidationException.php

--- src/Exceptions/ValidationException.php
+++ src/Exceptions/ValidationException.php
@@ -8,11 +8,16 @@ class ValidationException extends ResponseException
 {
     public function __construct(string $message = "", protected array $errors = [], int $code = 0, ?Throwable $previous = null)
     {
-        parent::__construct($message, $code, $previous);
+        parent::__construct(static::convertErrorsToMessage($this->errors), $code, $previous);
     }
 
     public function getErrors(): array
     {
         return $this->errors;
+    }
+
+    private static function convertErrorsToMessage(array $errors): string
+    {
+        return 'Validation failed: ' . implode('; ', array_map(fn ($array) => implode(', ', $array), $errors));
     }
 }
